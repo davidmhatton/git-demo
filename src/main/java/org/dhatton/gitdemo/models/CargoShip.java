f3e6c482a321093669b10eeb6b349d962435561c
David Hatton <dhatton@turnitin.com>
2019-11-12 13:54:36 +0000

    added exercise

--- src/main/java/org/dhatton/gitdemo/models/CargoShip.java
+++ src/main/java/org/dhatton/gitdemo/models/CargoShip.java
@@ -15,6 +15,7 @@ public class CargoShip extends Ship implements Navigable {
 	@Override
 	public void steerLeft(Double amount) {
 		// TODO: 2019-10-11 14:34 fill this in using a feature branch
+		// TODO: 2019-11-12 13:52 I will modify this whilst an update is being coded, and we can explore rebasing
 	}
 
 	@Override
--- src/test/java/org/dhatton/gitdemo/models/RudderTest.java
+++ src/test/java/org/dhatton/gitdemo/models/RudderTest.java
@@ -1,6 +1,6 @@
 package org.dhatton.gitdemo.models;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.runner.RunWith;
@@ -13,5 +13,5 @@ public class RudderTest {
 	void setUp() {
 	}
 
-	// TODO: 2019-10-28 08:52 Write these tests
+	// TODO: 2019-10-28 08:52 Write these tests in a new branch
 }
